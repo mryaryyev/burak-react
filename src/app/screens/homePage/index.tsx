cf62d0aa08b8c7bef8644bfa4165dc8440f9a9a9
ryan <mr.yaryyev@gmail.com>
2026-07-23 22:41:22 +0900

    fix: retrieve top users and modify ActiveUsers component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,20 +7,24 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setNewDishes, setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes, setTopUsers } =
+    actionDispatch(useDispatch());
 
   useEffect(() => {
     // Backend server data fetch => Data
@@ -32,10 +36,7 @@ export default function HomePage() {
         order: "productViews",
         productCollection: ProductCollection.DISH,
       })
-      .then((data) => {
-        // Slice: Data => Store
-        setPopularDishes(data);
-      })
+      .then((data) => setPopularDishes(data)) // Slice: Data => Store
       .catch((err) => console.log(err));
 
     product
@@ -45,10 +46,13 @@ export default function HomePage() {
         order: "createdAt",
         productCollection: ProductCollection.DISH,
       })
-      .then((data) => {
-        // Slice: Data => Store
-        setNewDishes(data);
-      })
+      .then((data) => setNewDishes(data)) // Slice: Data => Store
+      .catch((err) => console.log(err));
+
+    const member = new MemberService();
+    member
+      .getTopUsers()
+      .then((data) => setTopUsers(data)) // Slice: Data => Store
       .catch((err) => console.log(err));
 
     // @ts-ignore

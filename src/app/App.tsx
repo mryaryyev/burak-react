55eaa8cf9e4994d3aca547aa99f10f79c1992546
ryan <mr.yaryyev@gmail.com>
2026-07-29 08:01:32 +0900

    feat: create signup process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -36,6 +36,8 @@ function App() {
           onRemove={onRemove}
           onDelete={onDelete}
           onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
         />
       ) : (
         <OtherNavbar
@@ -44,6 +46,8 @@ function App() {
           onRemove={onRemove}
           onDelete={onDelete}
           onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
         />
       )}
       <Switch>
@@ -66,10 +70,10 @@ function App() {
       <Footer />
 
       <AuthenticationModal
-      signupOpen={signupOpen}
-      loginOpen={loginOpen}
-      handleLoginClose={handleLoginClose}
-      handleSignupClose={handleSignupClose}
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
       />
     </>
   );
